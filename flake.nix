b52b3deecd0ac549b160a5a90ad48a9d8f5ab132
Patrick Auernig <patrick.auernig@mykolab.com>
2025-12-01 17:52:19 +0100

    Add justfile for last solution

--- flake.nix
+++ flake.nix
@@ -1,43 +1,58 @@
 {
-    description = "Advent of Code";
+  description = "Advent of Code";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        rust-overlay = {
-            url = "github:oxalica/rust-overlay";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = { nixpkgs, rust-overlay, ... }:
+  outputs =
+    { nixpkgs, rust-overlay, ... }:
     let
-        lib = nixpkgs.lib;
-        systems = ["x86_64-linux"];
+      lib = nixpkgs.lib;
+      systems = [ "x86_64-linux" ];
 
-        forEachSystem = fn: lib.genAttrs systems (system:
-        let
+      forEachSystem =
+        fn:
+        lib.genAttrs systems (
+          system:
+          let
             overlays = [ (import rust-overlay) ];
             pkgs = import nixpkgs { inherit system overlays; };
-        in fn { inherit pkgs; } );
+          in
+          fn { inherit pkgs; }
+        );
 
-        mkRustToolchain = pkgs: pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-    in {
-        devShells = forEachSystem ({ pkgs }: let
-            rustToolchain = mkRustToolchain pkgs;
-            rustNightlyToolchain = pkgs.rust-bin.selectLatestNightlyWith (t: t.minimal.override {
-                extensions = [ "rustfmt" ];
-            });
+      mkRustToolchain = pkgs: pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    in
+    {
+      devShells = forEachSystem (
+        { pkgs }:
+        let
+          rustToolchain = mkRustToolchain pkgs;
+          rustNightlyToolchain = pkgs.rust-bin.selectLatestNightlyWith (
+            t:
+            t.minimal.override {
+              extensions = [ "rustfmt" ];
+            }
+          );
 
-        rustShell = pkgs.mkShell {
-                name = "AoC";
+          rustShell = pkgs.mkShell {
+            name = "AoC";
 
-                nativeBuildInputs = [
-                    rustToolchain
-                    rustNightlyToolchain
-                ];
-            };
-        in {
-            default = rustShell;
-        });
+            nativeBuildInputs = with pkgs; [
+              rustToolchain
+              rustNightlyToolchain
+              just
+            ];
+          };
+        in
+        {
+          default = rustShell;
+        }
+      );
     };
 }
